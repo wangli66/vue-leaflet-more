afeace620e54ae8349ef21f7fe8056da813bb989
wangli <wangli@geoway.com.cn>
2024-08-09 14:40:21 +0800

    部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,15 @@
-const nav = require("./config/nav.js");
-const sidebar = require("./config/sidebar.js");
+const nav = require('./config/nav.js');
+const sidebar = require('./config/sidebar.js');
 
 module.exports = {
-    title: "vue-leaflet-more 快速、便捷使用leaflet进行vue项目开发",
-    description: "vue-leaflet-more 快速、便捷使用leaflet进行vue项目开发",
-    dest: "./docs/dist",
+    title: 'vue-leaflet-more 快速、便捷使用leaflet进行vue项目开发',
+    description: 'vue-leaflet-more 快速、便捷使用leaflet进行vue项目开发',
+    // dest: "./docs/dist",
+    dest: './docsdist',
     base: process.env.NODE_ENV === 'production' ? '/vue-leaflet-more/' : '/',
-    plugins: [
-        "demo-container",
-        "@vuepress/plugin-back-to-top",
-        require("./collapse/index.js")
-    ],
+    plugins: ['demo-container', '@vuepress/plugin-back-to-top', require('./collapse/index.js')],
     themeConfig: {
-        logo: "https://vuejs.org/images/logo.png",
+        logo: 'https://vuejs.org/images/logo.png',
         sidebarDepth: 3,
         nav,
         sidebar: sidebar,
